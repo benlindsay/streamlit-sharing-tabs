1b2b8c2724430640e165621b84951120457c0449
Ben Lindsay <benjlindsay@gmail.com>
2021-07-31 18:56:17 -0500

    add links with query parameters

--- app.py
+++ app.py
@@ -3,3 +3,9 @@ import streamlit as st
 query_params = st.experimental_get_query_params()
 
 st.write(query_params)
+
+st.markdown("[/](/)")
+
+st.markdown("[/?foo=bar](/?foo=bar)")
+
+st.markdown("[https://share.streamlit.io/benlindsay/streamlit-sharing-tabs/main/app.py?foo=bar](https://share.streamlit.io/benlindsay/streamlit-sharing-tabs/main/app.py?foo=bar)")
