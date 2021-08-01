3475f5e142d333c6393939765c9712a72596aec6
Ben Lindsay <ben.lindsay@chrobinson.com>
2021-08-01 07:12:25 -0500

    add streamlit_url

--- app.py
+++ app.py
@@ -1,4 +1,8 @@
+import os
+
 import streamlit as st
+
+STREAMLIT_URL = os.getenv("STREAMLIT_URL", "/")
 
 query_params = st.experimental_get_query_params()
 
@@ -9,3 +13,5 @@ st.markdown("[/](/)")
 st.markdown("[/?foo=bar](/?foo=bar)")
 
 st.markdown("[https://share.streamlit.io/benlindsay/streamlit-sharing-tabs/main/app.py?foo=bar](https://share.streamlit.io/benlindsay/streamlit-sharing-tabs/main/app.py?foo=bar)")
+
+st.markdown(f"[{STREAMLIT_URL}?foo=bar]({STREAMLIT_URL}?foo=bar)")
