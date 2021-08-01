66686525e55018d3a7e6a5aa03831f6dba67a6c1
Ben Lindsay <benjlindsay@gmail.com>
2021-08-01 10:08:10 -0500

    add tabs

--- app.py
+++ app.py
@@ -6,12 +6,42 @@ STREAMLIT_URL = os.getenv("STREAMLIT_URL", "/")
 
 query_params = st.experimental_get_query_params()
 
-st.write(query_params)
+st.markdown(
+    '<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">',
+    unsafe_allow_html=True,
+)
+tabs = ["Home", "About", "Contact"]
+if "tab" in query_params:
+    active_tab = query_params["tab"][0]
+else:
+    active_tab = "Home"
 
-st.markdown("[/](/)")
+if active_tab not in tabs:
+    st.experimental_set_query_params(tab="Home")
+    active_tab = "Home"
 
-st.markdown("[/?foo=bar](/?foo=bar)")
+li_items = "".join(
+    f"""
+    <li class="nav-item">
+        <a class="nav-link{' active' if t==active_tab else ''}" href="{STREAMLIT_URL}?tab={t}">{t}</a>
+    </li>
+    """
+    for t in tabs
+)
+tabs_html = f"""
+    <ul class="nav nav-tabs">
+    {li_items}
+    </ul>
+"""
 
-st.markdown("[https://share.streamlit.io/benlindsay/streamlit-sharing-tabs/main/app.py?foo=bar](https://share.streamlit.io/benlindsay/streamlit-sharing-tabs/main/app.py?foo=bar)")
+st.markdown(tabs_html, unsafe_allow_html=True)
+st.markdown("<br>", unsafe_allow_html=True)
 
-st.markdown(f"[{STREAMLIT_URL}?foo=bar]({STREAMLIT_URL}?foo=bar)")
+if active_tab == "Home":
+    st.header("Home")
+elif active_tab == "About":
+    st.header("About")
+elif active_tab == "Contact":
+    st.header("Contact")
+else:
+    st.error("Something has gone terribly wrong.")
